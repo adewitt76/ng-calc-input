1ad6acb0790dd776fb77e102753c1696e731cf46
Aaron DeWitt <adewitt76@gmail.com>
2019-08-25 13:41:40 -0500

    added in module and directive

---
+++ projects/ng-calc-input/ng-calc-input.directive.spec.ts
@@ -0,0 +1,8 @@
+import { NgCalcInputDirective } from './ng-calc-input.directive';
+
+describe('NgCalcInputDirective', () => {
+  it('should create an instance', () => {
+    const directive = new NgCalcInputDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ projects/ng-calc-input/ng-calc-input.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[calc-input]'
+})
+export class NgCalcInputDirective {
+
+  constructor() { }
+
+}
