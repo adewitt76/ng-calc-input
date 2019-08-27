067380c6f8accdd7c3726f257d17dc5bbe5e8ce7
adewitt76 <adewitt76@gmail.com>
2019-08-27 07:04:17 -0500

    worked on number input and justify left

--- projects/ng-calc-input/src/lib/ng-calc-input.directive.ts
+++ projects/ng-calc-input/src/lib/ng-calc-input.directive.ts
@@ -8,47 +8,58 @@ export class NgCalcInputDirective {
 
   constructor(private el: ElementRef) {
     this.inputElement = el;
+    this.inputElement.nativeElement.style.textAlign = 'right';
   }
 
   @HostListener('keypress', ['$event']) onKeyPress(event: KeyboardEvent) {
     const key = event.key;
+    if (this.keyPressIsNotValid(key)) {
+      event.preventDefault();
+    }
+  }
+
+  private keyPressIsNotValid(key: string): boolean {
     const isNumber = new RegExp('[0-9]').test(key);
     const isPeriod = key === '.';
     const isNegative = key === '-';
-    const isFirstNumber = this.inputElement.nativeElement.selectionStart === 0;
-    const isZero = key === '0';
 
     if (isNumber === false &&
       isPeriod === false &&
       isNegative === false) {
-      event.preventDefault();
-      return;
+      return true;
     }
 
     if (isNegative) {
-      if (this.inputElement.nativeElement.selectionStart !== 0 ||
-        this.inputElement.nativeElement.value.includes('-')) {
-        event.preventDefault();
-        return;
+      if (!(this.inputElement.nativeElement.selectionStart === 0 ||
+          this.inputElement.nativeElement.selectionStart === null) ||
+          this.inputElement.nativeElement.value.includes('-') ||
+          this.inputElement.nativeElement.value < 0 ) {
+        return true;
       }
     }
 
     if (isPeriod) {
       if (this.inputElement.nativeElement.value.includes('.')) {
-        event.preventDefault();
-        return;
+        return true;
       }
     }
 
-    if (isFirstNumber && this.inputElement.nativeElement.value < 0) {
-      event.preventDefault();
+    return this.isSpecialRule(key);
+  }
+
+  private isSpecialRule(key: string): boolean {
+    const isLeadingCharacter = this.inputElement.nativeElement.selectionStart === 0;
+    const isZero = key === '0';
+
+    if (isLeadingCharacter && this.inputElement.nativeElement.value < 0) {
+      return true;
     }
 
     if (isZero) {
-      if (this.inputElement.nativeElement.value && isFirstNumber) {
-        event.preventDefault();
-        return;
+      if (this.inputElement.nativeElement.value && isLeadingCharacter) {
+        return true;
       }
     }
+    return false;
   }
 }
