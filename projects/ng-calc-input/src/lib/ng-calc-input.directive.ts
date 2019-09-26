36dabd29a6dc0663d68512e2f49e24abc2be2f38
Aaron DeWitt <Aaron.DeWitt@vu.com>
2019-09-26 14:03:58 -0500

    debug logging for android

--- projects/ng-calc-input/src/lib/ng-calc-input.directive.ts
+++ projects/ng-calc-input/src/lib/ng-calc-input.directive.ts
@@ -11,6 +11,12 @@ export class NgCalcInputDirective {
   }
 
   @HostListener('beforeinput', ['$event']) onBeforeInput(event: any) {
+    console.log('Event: ');
+    console.log(event);
+    console.log('inputElemnet');
+    console.log(this.inputElement);
+
+    console.log('event.data: ' + event.data);
     if (event.data === null) {
       return;
     }
@@ -22,14 +28,20 @@ export class NgCalcInputDirective {
 
   private keyPressIsNotValid(event: any): boolean {
     const maxIntegerLength = parseInt(this.calcFormat, 10);
+    console.log('maxIntegerLength: ' + maxIntegerLength);
     const precision = (this.calcFormat && this.calcFormat.includes('.')) ? parseInt(this.calcFormat.split('.')[1], 10) : 0;
+    console.log('precision: ' + precision);
 
     const nativeElement = this.inputElement.nativeElement as HTMLInputElement;
     const beginningSubstring = nativeElement.value.substring(0, nativeElement.selectionStart);
+    console.log('beginingSubstring: ' + beginningSubstring);
     const endSubstring = nativeElement.value.substring(nativeElement.selectionEnd, nativeElement.value.length);
+    console.log('endSubstring: ' + endSubstring);
     const parsedString = beginningSubstring + event.data + endSubstring;
+    console.log('parsedString: ' + parsedString);
 
-    // console.log(parsedString);
+    console.log('nativeElement start: ' + nativeElement.selectionStart + ' nativeElement end: ' + nativeElement.selectionEnd);
+    console.log('event start: ' + event.target.selectionStart + ' event end: ' + event.target.selectionEnd);
 
     // tslint:disable-next-line:max-line-length
     const regularExp =  precision && precision > 0 ? `^-?((\\d{0,${maxIntegerLength}}(\\.\\d{0,${precision}})?)|(\\.\\d{0,${precision}}))$` : `^-?(\\d{0,${maxIntegerLength}})$`;
